9b0140cd9fbad843838bdc85ea793b43cf6a4b13
Daniel-Paez-Rojas <danielpaez.web@gmail.com>
2024-11-25 20:17:34 +0100

    Se me olvidó crear rama

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import MainNavbar from './components/Navbar';
+import Home from './pages/Home';
+import AboutMe from './pages/AboutMe';
+import Portfolio from './pages/Portfolio';
+import Experience from './pages/Experience';
+import ContactMe from './pages/ContactMe';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <MainNavbar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" element={<AboutMe />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/contact" element={<ContactMe />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,26 @@
+import '../styles/navbar.scss';
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+
+function MainNavbar() {
+  return (
+    <Navbar collapseOnSelect expand="md" className="navbar">
+      <Container id='navbar-contenedor'>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About Me</Nav.Link>
+            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="#experience">Experience</Nav.Link>
+            <Nav.Link href="#contact">Contact Me</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default MainNavbar;
