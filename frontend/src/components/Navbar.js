338299a386825435bc26b0a7009d997e5786503b
Daniel-Paez-Rojas <danielpaez.web@gmail.com>
2024-11-29 17:10:45 +0100

    Navbar updated

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -12,9 +12,10 @@ function MainNavbar() {
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#about">About Me</Nav.Link>
+            <Nav.Link href="#qualification">Qualification</Nav.Link>
             <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="#experience">Experience</Nav.Link>
-            <Nav.Link href="#contact">Contact Me</Nav.Link>
+            <Nav.Link href="#skills">Skills</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
